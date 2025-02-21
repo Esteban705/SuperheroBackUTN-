21884b293c6184f2fdbbf08333fecb792bb66a64
Esteban <esteban9ateneo@gmail.com>
2025-02-20 23:21:55 -0300

    add creator and superhero

--- src/middleware/validator.js
+++ src/middleware/validator.js
@@ -10,3 +10,17 @@ exports.loginValidation = [
     check('email', 'Email inválido').isEmail(),
     check('password', 'La contraseña es requerida').exists()
 ];
+
+exports.superheroValidation = [
+    check('name', 'El nombre del superhéroe es requerido').not().isEmpty(),
+    check('alterEgo', 'El alter ego es requerido').not().isEmpty(),
+    check('powers', 'Los poderes son requeridos').isArray().notEmpty(),
+    check('description', 'La descripción es requerida').not().isEmpty(),
+    check('creator', 'El creador es requerido').isMongoId()
+];
+
+exports.creatorValidation = [
+    check('name', 'El nombre es requerido').not().isEmpty(),
+    check('company', 'La compañía es requerida').not().isEmpty(),
+    check('yearsOfExperience', 'Los años de experiencia son requeridos').isNumeric()
+]; 
